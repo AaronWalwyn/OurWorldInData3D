a7b3611cc60eed085c3e57018dedd8eee3fa02a6
Aaron Walwyn <aaronwalwyn@btinternet.com>
2020-06-18 00:10:34 +0100

    Updated rider settings

---
+++ Assets/Scripts/LatLong.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public struct LatLong
+{
+    [SerializeField] public float Lat;
+    [SerializeField] public float Long;
+        
+    public LatLong(float lat, float @long)
+    {
+        Lat = lat;
+        Long = @long;
+    }
+}
---
+++ Assets/Scripts/Location.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Location
+{
+    public string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+
+    public LatLong LatLon
+    {
+        get => _latLon;
+        set => _latLon = value;
+    }
+
+    public string Code
+    {
+        get => _code;
+        set => _code = value;
+    }
+
+    [SerializeField] private string _code;
+    [SerializeField] private string _name;
+    [SerializeField] private LatLong _latLon;
+}
